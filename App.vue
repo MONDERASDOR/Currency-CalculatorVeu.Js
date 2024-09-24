4ddd82c436b324b90ad1fa8b330be9cafebb132e
MONDERASDOR <140967226+MONDERASDOR@users.noreply.github.com>
2024-09-24 12:42:52 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Currency Converter</h1>
+    <input v-model="amount" placeholder="Amount" />
+    <select v-model="currency">
+      <option value="USD">USD</option>
+      <option value="EUR">EUR</option>
+    </select>
+    <button @click="convert">Convert</button>
+    <p v-if="result">Converted Amount: {{ result }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      amount: '',
+      currency: 'USD',
+      result: null,
+    };
+  },
+  methods: {
+    convert() {
+      const rates = {
+        USD: 1.0,
+        EUR: 0.85,
+      };
+      this.result = this.amount * rates[this.currency];
+    },
+  },
+};
+</script>
